ab44df91c2428b575de24f1448ec3d3b548133e1
robertino129 <robertino129@yahoo.com>
2014-01-18 20:12:48 +0200

    first functional SCARDF example
    first functional SCARDF example

--- hello-play/app/controllers/MessageController.scala
+++ hello-play/app/controllers/MessageController.scala
@@ -3,6 +3,9 @@ package controllers
 import play.api.mvc.{Action, Controller}
 import play.api.libs.json.Json
 import play.api.Routes
+import org.scardf._
+import org.joda.time.LocalDate
+
 
 case class Message(value: String)
 
@@ -25,4 +28,13 @@ object MessageController extends Controller {
     Ok(Routes.javascriptRouter("jsRoutes")(routes.javascript.MessageController.getMessage)).as(JAVASCRIPT)
   }
 
+  def postFuseki = Action {
+    val dc = Vocabulary( "http://purl.org/dc/elements/1.1/#" )
+    val homepage = UriRef( "http://www.example.org/index.html" )
+    val g = Graph( homepage -(  dc\"date" -> new LocalDate( 1999, 8, 16 ), dc\"language" -> "en",
+    dc\"creator" -> UriRef( "http://example.org/staffid/85740" )) )
+    val rez = g.rend
+    print(rez)
+    Ok(Json.toJson(Message("Ok!")))
+  }
 }
