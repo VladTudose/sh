c31cfe90b0c455c1357eeebbf319080aed929665
Vlad Tudosea <tudose_va@yahoo.com>
2014-02-09 11:42:51 +0200

    Fix typo.

--- hello-play/app/controllers/LocationController.scala
+++ hello-play/app/controllers/LocationController.scala
@@ -2,6 +2,6 @@ package controllers
 
 object LocationController extends CrudController {
     def domain = "locations"
-    def fields = Seq(("city", None), ("contry", None), ("latitude", None),
+    def fields = Seq(("city", None), ("country", None), ("latitude", None),
         ("longitude", None))
 }
