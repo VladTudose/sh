4e0d78bd518b5e0242aa9f7ad7dee4be2380dc0e
Vlad Tudosea <tudose_va@yahoo.com>
2014-02-10 04:21:22 +0200

    Partial work on DonationController

--- hello-play/app/controllers/DonationController.scala
+++ hello-play/app/controllers/DonationController.scala
@@ -8,9 +8,25 @@ object DonationController extends CrudController {
         
     override def links = Seq("warehouse")
     
+    // def getResQ(rqp: Seq[String], rqo: Seq[(String, String)]) = {
+    //     val resId = ((rqp zip rqo) filter { case (p, _) => p == "resource"})(0)._2._2
+    //     val quantity = ((rqp zip rqo) filter { case (p, _) => p == "quantity"})(0)._2._2
+    //     (resId, quantity)
+    // }
+    // 
     // override def doExtra(body: Map[String, Seq[String]]) = {
     //     val warehouseId = body("warehouse")(0)
+    //     val rqId = body("resourcequantity")(0)
+    //     
+    //     val (rqs, rqp, rqo) = ResourceQuantityController.getBindings(rqId)
+    //     
+    //     
     //     val (_, p, o) = WarehouseController.getBindings(warehouseId)
-    //     (p zips o._)
+    //     (p zips (o.unzip._2)) filter { 
+    //         case (p, o) => p == "inventory" }).unzip._2 map {
+    //         o => o.substring(ResourceQuantityController.lg)
+    //     } map {
+    //         o => (o, ResourceQuantityController.getBindings(o))
+    //     } filter { case (o, b) => 
     // }
 }
