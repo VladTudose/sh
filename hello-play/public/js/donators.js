e39deac19f6e378d3d052159e3188ec91c1c081b
Vlad Cristea <vld.cristea@gmail.com>
2014-02-09 20:23:09 -0800

    donators

---
+++ hello-play/public/js/donators.js
@@ -0,0 +1,70 @@
+	
+$(document).ready(function() {
+
+		function addToRow(row,value,link){
+			var row1 = $('<td onclick="document.location=\'http://localhost:9000/api'+link +'\'"></td>').addClass('bar').text(value);
+			row.append(row1);
+		}	
+		function deleteItem(link){
+				var url ="/api"+link;
+                var x = new XMLHttpRequest();
+                x.open("DELETE", url, true);
+                x.send(null);
+		}
+		function addToDeleteRow(row,item){
+			var row1 = $('<td ></td>').addClass('bar');
+			row.append(row1);
+			var deleteButton = $('<button id=\"'+ item._links.self.href +'\"></button>');
+			row1.append(deleteButton);
+			$(deleteButton).parent().parent().attr('id',   item._links.self.href);
+			$(deleteButton).click(function (event) {
+				deleteItem($(this).attr('id'));
+				$(this).parent().parent().remove();
+			});
+		}		
+		
+		$( document ).ready(function() {
+			render();
+		});
+			
+		function render() {
+		var table = $('<table id=\"data\"></table>').addClass('foo');
+			
+			row = $('<tr id=\"header\"></tr>');
+			table.append(row);
+			addToRow(row,"Lastname");
+			addToRow(row,"Firstname");
+			addToRow(row,"E-mail");
+			addToRow(row,"");
+				
+					
+			$.ajax({
+                    type: "GET",
+                    url: "http://localhost:9000/api/donators",
+                    acccept: "application/json",
+                    success: function(response) {
+							print(response);
+                        }
+                    })
+                
+				
+			
+			$('#container').append(table);
+			
+		}
+		function print(response){
+			for(var i=0;i<response._embedded.donators.length;i++)
+			{
+				var item = response._embedded.donators[i];
+				var link = item._links.self.href;
+				row = $('<tr ></tr>');
+				$("#data").append(row);
+				addToRow(row,item.lastname,link);
+				addToRow(row,item.firstname,link);
+				addToRow(row,item.email,link);
+				addToDeleteRow(row,item);
+			}
+		}
+	
+	
+});
